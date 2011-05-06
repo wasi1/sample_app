623b39f277e80dbbe2bd1095e4c5beb285aa8b1c
wasi ahmed <wasi.cjr@gmail.com>
2011-05-06 13:15:08 -0400

    Added a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+   # Return a title on a per-page basis
+   def title
+	base_title = "eceFTW"
+	if @title.nil?
+		base_title
+	else
+		"#{base_title}|#{@title}"
+	end
+   end
+
+
 end
